1e4b0b6060f91cf5066ac1a914d874793542bf6f
lupecasas119 <81210097+lupecasas119@users.noreply.github.com>
2021-03-25 21:43:05 -0600

    Create init.js

---
+++ init.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAKIRGQshDYDsvGtHaEEfe2zCGaiBCcKrs",
+    authDomain: "autentifica42mgch.firebaseapp.com",
+    projectId: "autentifica42mgch",
+    storageBucket: "autentifica42mgch.appspot.com",
+    messagingSenderId: "157068818151",
+    appId: "1:157068818151:web:19431bde941ed5538f1442",
+    measurementId: "G-EDV05D9XF5"
+  };
